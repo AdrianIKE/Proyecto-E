0901a5798b94f2a22c59e87556006b2325eb89ab
Adrian Gutierrez <adgutierrez@ikeasistencia.com>
2023-03-29 21:04:48 -0600

    Los resultados de los examenes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { sequelize } from "./database/database.js";
 import express from "express";
 import rutas from "./routes/reactivos.routes.js"
-
+import examenRutas from "./routes/examen.routes.js"
 async function main(){
     try {
         await sequelize.sync({force:false})
@@ -17,6 +17,7 @@ async function main(){
     app.use(express.json())
     app.use(express.urlencoded({ extended:false}));
     app.use(rutas)
+    app.use(examenRutas)
     app.listen(3000)
 
     console.log("El servidor escucha en puerto 3000")
